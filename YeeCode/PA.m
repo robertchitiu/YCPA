c1fd37677121c39367d03c677dfde2619cd349b5
robertchitiu <77218492+robertchitiu@users.noreply.github.com>
2021-03-26 19:03:12 -0400

    Add files via upload

---
+++ YeeCode/PA.m
@@ -0,0 +1,9 @@
+%{
+  Essentially what is happening in this code is that it is showing 
+ and electric field and its resulting magnetic field propicating 
+through some material. In this material there is a high.
+ The boundary conditions set where the wave begins and how the reflect off
+ the sides if they do or if they dissapate. Setting the BC to e mean that
+ the sides will not reflect the wave. Setting ST to -0.05 raised the
+ intensity of the wave making it much easier to tell what is happening
+%}
